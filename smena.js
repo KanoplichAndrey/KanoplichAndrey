c72fd592db6ce253b17a9e94b731db8b713b6cab
KanoplichAndrey <kanop0000@gmail.com>
2021-09-03 12:35:00 +0300

    gf

--- smena.js
+++ smena.js
@@ -2,7 +2,7 @@ let chet = 1;
 			window.onload = function () { setTimeout(frout, 4000); }
 			function frout() {
 			chet++;
-			if (chet>5) {chet=0;}
+			if (chet>2) {chet=0;}
 				switch(chet) {
 				case 1:
 					div.style.cssText=` background:  url('cuba.jpg') center top no-repeat;`
@@ -10,15 +10,7 @@ let chet = 1;
 				case 2:
 					div.style.cssText=` background:  url('gavana-i-znamenitiy-retro-avtomobil.jpg') center top no-repeat;`
 					break;
-				case 3:
-					div.style.cssText=` background:  url('3.jpg') left top no-repeat;`
-					break;
-            case 4:
-               div.style.cssText=` background:  url('4.jpg') right top no-repeat;`
-               break;
-            case 5:
-               div.style.cssText=` background:  url('5.jpg') left top no-repeat;`
-               break;      
+				
 
 				}
 			setTimeout(frout, 4000);
