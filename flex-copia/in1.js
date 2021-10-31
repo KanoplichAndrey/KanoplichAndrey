6006460f273121a7e5e873194264feb332b13e8a
KanoplichAndrey <kanop0000@gmail.com>
2021-11-01 02:18:21 +0300

    ggg

--- flex-copia/in1.js
+++ flex-copia/in1.js
@@ -1,7 +1,7 @@
-el=document.getElementById(id);
-el.src=in1;
+// el=document.getElementById(id);
+// el.src=in1;
 
-el=document.getElementById(id);
-el.src=in2;
-el=document.getElementById(id);
-el.src=in3;
+// el=document.getElementById(id);
+// el.src=in2;
+// el=document.getElementById(id);
+// el.src=in3;
