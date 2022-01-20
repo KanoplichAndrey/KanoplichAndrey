1330f4b543f02aba44da6711dfa6e89fdc8c936e
KanoplichAndrey <kanop0000@gmail.com>
2022-01-21 00:30:11 +0300

    ghhh

--- menu.js
+++ menu.js
@@ -1,72 +1,72 @@
-let menuElem = document.getElementById('sweeties');
-let titleElem = menuElem.querySelector('.title');
+// let menuElem = document.getElementById('sweeties');
+// let titleElem = menuElem.querySelector('.title');
 
-titleElem.onclick = function() {
-menuElem.classList.toggle('open');
-};
+// titleElem.onclick = function() {
+// menuElem.classList.toggle('open');
+// };
 
-let menuElem2 = document.getElementById('sweeties2');
-let titleElem2 = menuElem2.querySelector('.title');
+// let menuElem2 = document.getElementById('sweeties2');
+// let titleElem2 = menuElem2.querySelector('.title');
 
-titleElem2.onclick = function() {
-menuElem2.classList.toggle('open');
-};
+// titleElem2.onclick = function() {
+// menuElem2.classList.toggle('open');
+// };
 
-let menuElem3 = document.getElementById('sweeties3');
-let titleElem3 = menuElem3.querySelector('.title');
+// let menuElem3 = document.getElementById('sweeties3');
+// let titleElem3 = menuElem3.querySelector('.title');
 
-titleElem3.onclick = function() {
-menuElem3.classList.toggle('open');
-};
+// titleElem3.onclick = function() {
+// menuElem3.classList.toggle('open');
+// };
 
-let menuElem4 = document.getElementById('sweeties4');
-let titleElem4 = menuElem4.querySelector('.title');
+// let menuElem4 = document.getElementById('sweeties4');
+// let titleElem4 = menuElem4.querySelector('.title');
 
-titleElem4.onclick = function() {
-menuElem4.classList.toggle('open');
-};
+// titleElem4.onclick = function() {
+// menuElem4.classList.toggle('open');
+// };
 
-let menuElem5 = document.getElementById('sweeties5');
-let titleElem5 = menuElem5.querySelector('.title');
+// let menuElem5 = document.getElementById('sweeties5');
+// let titleElem5 = menuElem5.querySelector('.title');
 
-titleElem5.onclick = function() {
-menuElem5.classList.toggle('open');
-};
+// titleElem5.onclick = function() {
+// menuElem5.classList.toggle('open');
+// };
 
-let menuElem6 = document.getElementById('sweeties6');
-let titleElem6 = menuElem6.querySelector('.title');
+// let menuElem6 = document.getElementById('sweeties6');
+// let titleElem6 = menuElem6.querySelector('.title');
 
-titleElem6.onclick = function() {
-menuElem6.classList.toggle('open');
-};
+// titleElem6.onclick = function() {
+// menuElem6.classList.toggle('open');
+// };
 
-let menuElem7 = document.getElementById('sweeties7');
-let titleElem7 = menuElem7.querySelector('.title');
+// let menuElem7 = document.getElementById('sweeties7');
+// let titleElem7 = menuElem7.querySelector('.title');
 
-titleElem7.onclick = function() {
-menuElem7.classList.toggle('open');
-};
+// titleElem7.onclick = function() {
+// menuElem7.classList.toggle('open');
+// };
 
-let menuElem8 = document.getElementById('sweeties8');
-let titleElem8 = menuElem8.querySelector('.title');
+// let menuElem8 = document.getElementById('sweeties8');
+// let titleElem8 = menuElem8.querySelector('.title');
 
-titleElem8.onclick = function() {
-menuElem8.classList.toggle('open');
-};
+// titleElem8.onclick = function() {
+// menuElem8.classList.toggle('open');
+// };
 
-let menuElem9 = document.getElementById('sweeties9');
-let titleElem9 = menuElem9.querySelector('.title');
+// let menuElem9 = document.getElementById('sweeties9');
+// let titleElem9 = menuElem9.querySelector('.title');
 
-titleElem9.onclick = function() {
-menuElem9.classList.toggle('open');
-};
+// titleElem9.onclick = function() {
+// menuElem9.classList.toggle('open');
+// };
 
-let menuElem10 = document.getElementById('sweeties10');
-let titleElem10 = menuElem10.querySelector('.title');
+// let menuElem10 = document.getElementById('sweeties10');
+// let titleElem10 = menuElem10.querySelector('.title');
 
-titleElem10.onclick = function() {
-menuElem10.classList.toggle('open');
-};
+// titleElem10.onclick = function() {
+// menuElem10.classList.toggle('open');
+// };
 
 let menuElem11 = document.getElementById('sweeties11');
 let titleElem11 = menuElem11.querySelector('.title');
