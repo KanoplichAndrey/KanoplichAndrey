6b905bba217c355c51154274f3b54b2437fd14f9
KanoplichAndrey <kanop0000@gmail.com>
2022-04-29 19:59:45 +0300

    jjj

--- js/dz1/ANKETA/projekt/js.js
+++ js/dz1/ANKETA/projekt/js.js
@@ -315,51 +315,7 @@ function reset() {
 
 
     function formMenu() {
-        document.addEventListener('touchstart', handleTouchStart, false);
-document.addEventListener('touchmove', handleTouchMove, false);
-var xDown = null;
-var yDown = null;
- 
-function getTouches(evt) {
-return evt.touches || // чистый API JS
-evt.originalEvent.touches; // jQuery
-}
- 
-function handleTouchStart(evt) {
-const firstTouch = getTouches(evt)[0];
-xDown = firstTouch.clientX;
-yDown = firstTouch.clientY;
-};
- 
-function handleTouchMove(evt) {
-if ( ! xDown || ! yDown ) {
-return;
-}
- 
-var xUp = evt.touches[0].clientX;
-var yUp = evt.touches[0].clientY;
- 
-var xDiff = xDown - xUp;
-var yDiff = yDown - yUp;
- 
-if ( Math.abs( xDiff ) &gt, Math.abs( yDiff ) ) {/* отлавливаем разницу в движении */
-if ( xDiff &gt, 0 ) {
-/* swipe влево */
-} else {
-/* swipe вправо */
-}
-} else {
-if ( yDiff &gt, 0 ) {
-/* swipe вверх */
-} else {
-/* swipe вниз */
-}
-}
-/* свайп был, обнуляем координаты */
-xDown = null;
-yDown = null;
-};
-
+       
 
         if (window.innerWidth < 1160) {
             form.style.transition = 3 + 's'
